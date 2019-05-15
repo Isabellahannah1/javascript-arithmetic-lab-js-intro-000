7973ad924aa88b2a7323d4239ba6d5dcae697c53
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-15 16:43:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-function add(a, b) {
-  return (a + b);
+function add(a, b){
+  return a + b;
 }
-function subtract(a, b) {
-  return (a - b);
+
+function subtract(a, b){
+  return a - b;
 }
-function multiply(a, b) {
-  return (a * b);
+
+function multiply(a, b){
+  return a * b;
 }
-function divide(a, b) {
-  return (a / b);
+
+function divide(a, b){
+  return a / b;
 }
-function inc(a) {
-return (a + 1);
+
+function inc(n){
+  n++;
+  return n;
 }
-function dec(a) {
-  return (a - 1);
+
+function dec(n){
+  n--;
+  return n;
 }
-function makeInt(n) {
-return (parseInt('n'
-));
+
+function makeInt(n){
+  return (parseInt(n));
 }
-function makeInt(n) {
-  return parseInt(n, 10);
+
+function makeInt(n){
+  return (parseInt(n, 10));
 }
-function preserveDecimal(n) {
-return parseFloat(n);
+
+function preserveDecimal(n){
+  return (parseFloat(n));
 }
